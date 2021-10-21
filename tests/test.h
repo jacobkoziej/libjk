904e534f8c22bc60be786150b702ddb6bd95ead8
Jacob Koziej <jacobkoziej@gmail.com>
2021-10-21 01:35:02 -0400

    Add a common test header

---
+++ tests/test.h
@@ -0,0 +1,38 @@
+/*
+ * test.h -- common test header
+ * Copyright (C) 2021  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIBJK_TESTS_TEST_H_
+#define LIBJK_TESTS_TEST_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <setjmp.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#include <cmocka.h>
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* LIBJK_TESTS_TEST_H_ */
