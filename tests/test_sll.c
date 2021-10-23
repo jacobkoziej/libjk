05e579b912891ea4fc11f7b9e19433dc56f5f241
Jacob Koziej <jacobkoziej@gmail.com>
2021-10-22 22:50:03 -0400

    Add test for jk_sll_init()

---
+++ tests/test_sll.c
@@ -0,0 +1,49 @@
+/*
+ * test_sll.c -- singly-linked list (test)
+ * Copyright (C) 2021  Jacob Koziej <jacobkoziej@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <jk/sll.h>
+#include "sll.h"
+
+#include <stddef.h>
+
+#include "test.h"
+#include "wrap_calloc.h"
+
+
+static void test_jk_sll_init(void **state)
+{
+	(void) state;
+	jk_sll_t *sll = NULL;
+
+
+	/* failed calloc() */
+	g_wrap_calloc_fail = 0;
+	sll = jk_sll_init();
+	assert_null(sll);
+}
+
+
+int main(void)
+{
+	static const struct CMUnitTest tests[] = {
+		cmocka_unit_test(test_jk_sll_init),
+	};
+
+
+	return cmocka_run_group_tests(tests, NULL, NULL);
+}
