84c63c549f4efde25e1c809d02b9a4a14949436d
Jacob Koziej <jacobkoziej@gmail.com>
2023-01-08 17:03:57 -0500

    Add `sll.h`

---
+++ include/jk/sll.h
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+/*
+ * sll.h -- singly linked list
+ * Copyright (C) 2023  Jacob Koziej <jacobkoziej@gmail.com>
+ */
+
+#ifndef LIBJK_SLL_H
+#define LIBJK_SLL_H
+
+
+#endif  /* LIBJK_SLL_H */
